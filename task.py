68705c1e0dc074f07ecec6d8c3b6a854a5f4709c
Tommi Holmgren <tommi@robocorp.com>
2023-06-06 11:05:13 +0300

    first commit

---
+++ task.py
@@ -0,0 +1,36 @@
+"""Template robot with Python."""
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+import torch
+
+model = "tiiuae/falcon-7b-instruct"
+
+def minimal_task():
+    print("\n---- Starting. ----\n")
+
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True,
+        device_map="auto",
+    )
+
+    sequences = pipeline(
+    "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
+        max_length=200,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+    )
+
+    for seq in sequences:
+        print(f"Result: {seq['generated_text']}")
+
+    print("\n---- Finished. ----\n")
+
+if __name__ == "__main__":
+    minimal_task()
